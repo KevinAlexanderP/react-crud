b8502cb7462396499aee3973f289a3cf52881169
KevinAlexanderP <kennedyisdead13@gmail.com>
2020-08-25 22:56:21 -0500

    all

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -1,73 +1,103 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase";
+import { toast } from "react-toastify";
 
+const LinksForm = (props) => {
+  const initialStateValues = {
+    url: "",
+    name: "",
+    description: "",
+  };
 
-const LinkForm = (props) => {
-    const initialStateValues = {
-        url:"",
-        name: "" ,
-        description:"", 
+  const [values, setValues] = useState(initialStateValues);
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const validURL = (str) => {
+    var pattern = new RegExp(
+      "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+        "(\\#[-a-z\\d_]*)?$",
+      "i"
+    ); // fragment locator
+    return !!pattern.test(str);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validURL(values.url)) {
+      return toast("invalid url", { type: "warning", autoClose: 1000 });
     }
-    
-    const [values, setValues] = useState(initialStateValues);
-    
-    const handleInputChange= e=> {
 
-        const {name,value} = e.target
+    props.addOrEditLink(values);
+    setValues({ ...initialStateValues });
+  };
 
-        setValues({...values,[name]:value})
+  const getLinkById = async (id) => {
+    const doc = await db.collection("links").doc(id).get();
+    setValues({ ...doc.data() });
+  };
 
-   
+  useEffect(() => {
+    if (props.currentId === "") {
+      setValues({ ...initialStateValues });
+    } else {
+      getLinkById(props.currentId);
     }
-    
-    const handleSubmit = (e) =>{
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.currentId]);
 
-        e.preventDefault();
-        props.addOrEditLink(values)
-        setValues({...initialStateValues})
-
-        console.log(values)
-        
-    }
-
-    return(
-        <form action="" className="card card-body " onSubmit={handleSubmit}>
-            <div className="form-group input-group">
-                <div className="bg-ligth input-group-text">
-                <i className="material-icons">insert_link</i>
-                </div>
-                        <input type="text" 
-                        className="form-control" 
-                        placeholder="https" 
-                        name="url"
-                        onChange={handleInputChange} value={values.url}/>
-                </div> 
-                
-                <div className="form-group input-group">
-                    <div className="bg-ligth input-group-text">
-                        <i className="material-icons">create</i>
-                    </div>
-                            <input type="text" 
-                            className="form-control" 
-                            placeholder="name" 
-                            name="name" onChange={handleInputChange} value={values.name}/>
-                </div>
-
-        <div className="form-group ">
-
-                <textarea name="description"   rows="3" className="form-control" placeholder="write a description" onChange={handleInputChange} value={values.description}>
-
-                </textarea>
-
+  return (
+    <form onSubmit={handleSubmit} className="card card-body border-primary">
+      <div className="form-group input-group">
+        <div className="input-group-text bg-light">
+          <i className="material-icons">insert_link</i>
         </div>
-        <button className="btn btn-primary">
-            
-            Save
-        </button>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="https://someurl.xyz"
+          value={values.url}
+          name="url"
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="form-group input-group">
+        <div className="input-group-text bg-light">
+          <i className="material-icons">create</i>
+        </div>
+        <input
+          type="text"
+          value={values.name}
+          name="name"
+          placeholder="Website Name"
+          className="form-control"
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="form-group">
+        <textarea
+          rows="3"
+          className="form-control"
+          placeholder="Write a Description"
+          name="description"
+          value={values.description}
+          onChange={handleInputChange}
+        ></textarea>
+      </div>
 
-        
+      <button className="btn btn-primary btn-block">
+        {props.currentId === "" ? "Save" : "Update"}
+      </button>
     </form>
-)
-}
+  );
+};
 
-export default LinkForm;
+export default LinksForm;
